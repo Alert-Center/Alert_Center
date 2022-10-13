fc5cde4a01a4adba3e507b61dd4b88b742f7c206
MuriloSBarbosa <murilo.barbosa@sptech.school>
2022-10-12 22:08:12 -0300

    UPDATE - css do simulador financeiro e codigo js

--- Site Institucional/js/script.js
+++ Site Institucional/js/script.js
@@ -31,9 +31,9 @@ function analisar_senha() {
 function entrar() {
     var email = in_email.value
     var senha = in_senha.value
-    
+
     if (email == "admin@sptech.school" && senha == "@Sptech2022") {
-        windown.location.href="dashboard-geral.html"
+        windown.location.href = "dashboard-geral.html"
     } else {
         alert(`Conta ainda não cadastrada!`)
     }
@@ -67,8 +67,8 @@ function criarSimulacao() {
     //Custo da nossa solução
     var solucaoinstalacao = racks * 300; //300 reais por Rack (200 equipamento + 100 mão de obra)
     var solucaoMensal = 600; //Custo do software de 600 reais mensais
-    var pacoteAnual = 600 * 12; //Custo do software em um ano
-    var solucaoAnual = solucaoinstalacao + pacoteAnual; //Custo da solução em um ano; 
+    var licenca_anual = 600 * 12; //Custo do software em um ano
+    var solucaoAnual = solucaoinstalacao + licenca_anual; //Custo da solução em um ano; 
 
     //Economia do Cliente é o quanto ele perde pelo quanto nossa solução custa (ou seja, o quanto ele evita de gastar)
     var economiaMes1 = prejuizoMes - (solucaoMensal + solucaoinstalacao); //Primeiro mês há o custo da instalação
@@ -81,74 +81,111 @@ function criarSimulacao() {
         alert("Preencha todos os campos!");
     } else {
 
+        // Configuração para formatar números pelo método Intl.NumberFormat. https://criarprogramas.com/2021/06/05/formatando-numeros-em-javascript-com-intl-numberformat/#:~:text=Usando%20o%20par%C3%A2metro%20options&text=style%3A%20estilo%20do%20formato%20a,o%20padr%C3%A3o%20%C3%A9%20%22decimal%22%20.
+        const options = { style: 'currency', currency: 'BRL', minimumFractionDigits: 2, maximumFractionDigits: 2 }
+        const formatNumber = new Intl.NumberFormat('pt-BR', options)
+
         // Código para inserir a div com o resultado da conta do simulador
         container_simulador.innerHTML =
             `
+        <div class="container container-solucao" id="container_simulador">
         <div class="content-simulador">
-            <div class="indicadores">
-                <h2>Prejuizo</h2>
-                <div class="indicador">
-                    <div class="title">
-                        <h2>Mensal</h2>
-                        <img src="img/bx_trending-down.png">
-                    </div>
-                    <div class="number">R$${prejuizoMes.toFixed(2)}</div>
-                </div>
-                <div class="indicador">
-                    <div class="title">
-                        <h2>Anual</h2>
-                        <img src="img/carbon_idea.png">
-                    </div>
-                    <div class="number">R$${prejuizoAno.toFixed(2)}</div>
-                </div>
-            </div>
+            <div class="container">
 
-            <div class="indicadores">
-                <h2>solucao</h2>
-                <div class="indicador">
-                    <div class="title">
-                        <h2>Instalação</h2>
-                        <img src="img/bx_trending-down.png">
+                <div class="indicadores">
+                    <h1>Prejuízo</h1>
+                    <div class="indicador">
+                        <div class="title">
+                            <h2>Mensal</h2>
+                        </div>
+                        <div id="div_prejuizoMes" class="number prejuizo">${formatNumber.format(prejuizoMes)}</div>
                     </div>
-                    <div class="number">R$${solucaoinstalacao.toFixed(2)}</div>
-                </div>
-                <div class="indicador">
-                    <div class="title">
-                        <h2>Pacote Anual</h2>
-                        <img src="img/carbon_idea.png">
+                    <div class="indicador">
+                        <div class="title">
+                            <h2>Anual</h2>
+                        </div>
+                        <div id="div_prejuizoAno" class="number prejuizo">${formatNumber.format(prejuizoAno)}</div>
                     </div>
-                    <div class="number">R$${pacoteAnual.toFixed(2)}</div>
                 </div>
-            </div>
 
-            <div class="indicadores">
-                <h2>economia</h2>
-                <div class="indicador">
-                    <div class="title">
-                        <h2>1º mes</h2>
-                        <img src="img/bx_trending-down.png">
+                <div class="indicadores">
+                    <h1>Solução</h1>
+                    <div class="indicador">
+                        <div class="title">
+                            <h2>Instalação</h2>
+                        </div>
+                        <div id="div_solucaoinstalacao" class="number">${formatNumber.format(solucaoinstalacao)}</div>
                     </div>
-                    <div class="number">R$${economiaMes1.toFixed(2)}</div>
+                    <div class="indicador">
+                        <div class="title">
+                            <h2>licença Anual</h2>
+                        </div>
+                        <div id="div_licenca_anual" class="number">${formatNumber.format(licenca_anual)}</div>
+                    </div>
                 </div>
-                <div class="indicador">
-                    <div class="title">
-                        <h2>demais meses</h2>
-                        <img src="img/bx_trending-down.png">
+
+                <div class="indicadores">
+                    <h1>Economia</h1>
+                    <div class="indicador">
+                        <div class="title">
+                            <h2>Mensal*</h2>
+                        </div>
+                        <div id="div_economiaMeses" class="number">${formatNumber.format(economiaMeses)}</div>
                     </div>
-                    <div class="number">R$${economiaMeses.toFixed(2)}</div>
-                </div>
-                <div class="indicador">
-                    <div class="title">
-                        <h2>Anual</h2>
-                        <img src="img/carbon_idea.png">
+                    <div class="indicador">
+                        <div class="title">
+                            <h2>Anual</h2>
+                        </div>
+                        <div id="div_economiaAno" class="number">${formatNumber.format(economiaAno)}</div>
                     </div>
-                    <div class="number">R$${economiaAno.toFixed(2)}</div>
                 </div>
             </div>
         </div>
 
-    <button class="btn btn-refazer" onclick="refazerSimulacao()">Refazer Simulação</button>
-    `;
+        <div class="content-legenda">
+            <div class="container">
+                <div class="legenda">
+                    <img src="img/simulador/bx_trending-down.png">
+                    <p><span>Prejuízo: </span> compreende os gastos advindos dos downtimes.
+                    <p>
+                </div>
+                <div class="legenda">
+                    <img src="img/simulador/carbon_idea.png">
+                    <p><span>Solução: </span>engloba a análise de infraestutura, instalação de equipamentos e a
+                        licença
+                        anual para acesso do sistema de monitoramento</p>
+                </div>
+                <div class="legenda">
+                    <img src="img/simulador/bx_trending-up.png">
+                    <p><span>Economia:</span> Representa o montante de gastos que foram poupados.</p>
+                </div>
+                <div class="legenda">
+                    <img src="img/simulador/icons8-ponto-de-exclamação-64.png">
+                    <p><span> * No 1º mês</span> o valor será de ${formatNumber.format(economiaMes1)} por incluir o preço da instalação.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <button class="btn btn-refazer" onclick="refazerSimulacao()">Refazer Simulação</button>
+    </div>
+
+        `;
+
+
+        // Adicionar uma cor ao número dependendo se a economia for positiva ou negativa
+        if (economiaMeses < 0) {
+            div_economiaMeses.classList.add("prejuizo");
+        } else {
+            div_economiaMeses.classList.add("economia");
+        }
+        if (economiaAno < 0) {
+            div_economiaAno.classList.add("prejuizo");
+        } else {
+            div_economiaAno.classList.add("economia");
+        }
+
+
     }
 }
 
