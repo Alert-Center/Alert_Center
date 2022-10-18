c74c8f27b9046303afdf06ea16103b58128a1cf9
luizfiller <luiz.fsilva@sptech.school>
2022-10-17 23:50:57 -0300

    Update - alterando HTML e CSS, add função JS para validação de senha, email e suas mensagens

--- Site Institucional/js/cadastro.js
+++ Site Institucional/js/cadastro.js
@@ -1,3 +1,34 @@
+// Função para fazer a validação do e-mail
+function analisar_email() {
+  var email = in_form_email.value;
+
+  var emailCorreto = email.indexOf("@" && ".") >= 0;
+
+  if (!emailCorreto) {
+    msg_email.innerHTML = `Insira seu email neste formato: <strong style="color:red;">nome@exemplo.com!</strong>`;
+  } else {
+    msg_email.innerHTML = ``;
+  }
+}
+
+// Função para fazer a validação da senha
+function analisar_senha() {
+  var senha = in_form_senha.value 
+  var confirmacaoSenha = in_form_confirmarSenha.value
+  // A expressão utilizada abaixo, serve para validar a string, a qual deve conter no mínimo 1 número, 1 letra minúscula, 1 letra maiúscula, 1 caracter especial, além de conter no mínimo 9 caracteres e no máximo 20
+  // A função ".match()" serve para pesquisar na string a expressão supracitada
+  var senhaCaracteres =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{9,20}$/;
+  var senhaCorreta = senha.match(senhaCaracteres);
+  var confirmacaoSenhaCorreta = confirmacaoSenha.match(senhaCaracteres);
+
+  if (!senhaCorreta || !confirmacaoSenhaCorreta) {
+    msg_senha.innerHTML = `A senha deve conter mais de 8 caracteres, contendo <strong>números</strong>, <strong>caracteres especiais</strong>, <strong>letras maiúsculas</strong> e <strong>minúsculas</strong>.`;
+  } else {
+    msg_senha.innerHTML = ``;
+  }
+}
+
 // OBSERVAÇÕES
 //parentElement: elemento pai (elemento que a tag está dentro)
 //LastElementChild: ultimo elemento filho (última tag que está dentro dele)
