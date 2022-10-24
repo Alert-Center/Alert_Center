b6a83123fea7e08f56a98739c365f923ca4cfd24
iury02santos <iury.santos@sptech.school>
2022-10-24 15:36:26 -0300

    UPDATE - Comentando partes desnecessárias -arduino

--- API/arduino/Arduino_5_Sensores/Arduino_5_Sensores.ino
+++ API/arduino/Arduino_5_Sensores/Arduino_5_Sensores.ino
@@ -4,17 +4,17 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include "DHT.h"
-#define DHTPIN A3 //definindo variáveis e a localização do pin
-#define LM35PIN A1 
-#define LUMIPIN A0
-#define CHAVPIN 7
+#define DHTPIN A0 //definindo variáveis e a localização do pin
+// #define LM35PIN A1 
+// #define LUMIPIN A4
+// #define CHAVPIN 7
 
 DHT dht(DHTPIN, DHT11); //uma variável que está sendo puxada da biblioteca
 
 void setup() //comando para iniciar o programa
 {
 pinMode(DHTPIN, INPUT); //declarando que a porta do DHTPIN é para entrada de dados
-pinMode(CHAVPIN, INPUT);
+// pinMode(CHAVPIN, INPUT);
 Serial.begin(9600); //definindo que a comunicação será em 9600bps(bits per seconds)
 dht.begin(); //dando start na biblioteca do DHT
 }
@@ -30,24 +30,24 @@ Serial.print(";");
 // Serial.print("U-Temp:"); 
 Serial.print(dht11_temperatura);
 Serial.print(";");
-float luminosidade = analogRead(LUMIPIN);
-Serial.print(luminosidade);
-Serial.print(";");
-float lm35_temperatura = analogRead(LM35PIN);
-lm35_temperatura = lm35_temperatura * 0.00488; // operação para transformar pulso em temperatura | '0.00488' é a voltagem de pulsos que o sensor capta
-lm35_temperatura = lm35_temperatura * 100;
-// Serial.print("Temperatura:");
-Serial.print(lm35_temperatura);
-Serial.print(";");
-int chave = digitalRead(7);
-if (chave == 0)
-{
-Serial.print("1");
-}
-else
-{
-Serial.print("0");
-}
-Serial.println();//pula uma linha para ficilitar a leitura
-delay(2000); //fica no loop e tem delay de 2 segundos para recomeçar o loop
+// float luminosidade = analogRead(LUMIPIN);
+// Serial.print(luminosidade);
+// Serial.print(";");
+// float lm35_temperatura = analogRead(LM35PIN);
+// lm35_temperatura = lm35_temperatura * 0.00488; // operação para transformar pulso em temperatura | '0.00488' é a voltagem de pulsos que o sensor capta
+// lm35_temperatura = lm35_temperatura * 100;
+// // Serial.print("Temperatura:");
+// Serial.print(lm35_temperatura);
+// Serial.print(";");
+// int chave = digitalRead(7);
+// if (chave == 0)
+// {
+// Serial.print("1");
+// }
+// else
+// {
+// Serial.print("0");
+// }
+ Serial.println();//pula uma linha para ficilitar a leitura
+ delay(2000); //fica no loop e tem delay de 2 segundos para recomeçar o loop
 }
