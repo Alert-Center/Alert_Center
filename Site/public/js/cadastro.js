5b7f34e5612ac41a04e7ce6993bc3231e72dd49a
leodanave <leonardo.nave@sptech.school>
2022-11-16 23:22:02 -0300

    ADD - API Web Data Viz

--- Site/public/js/cadastro.js
+++ Site/public/js/cadastro.js
@@ -250,3 +250,70 @@ function eyes() {
     img_eye_confirmar.src = "img/hidden.png";
   }
 }
+
+function cadastrar() {
+  aguardar();
+
+  //Recupere o valor da nova input pelo nome do id
+  // Agora vá para o método fetch logo abaixo
+  var nomeVar = in_form_nome.value;
+  var CNPJVar = in_form_CNPJ.value;
+  var telefone1Var = in_form_telefone.value;
+  var telefone2Var = in_form_celular.value;
+  var emailVar = in_form_email.value;
+  var senhaVar = in_form_senha.value;
+  var confirmacaoSenhaVar = in_form_confirmarSenha.value;
+
+  if (nomeVar == "" || emailVar == "" || senhaVar == "" || confirmacaoSenhaVar == "") {
+      cardErro.style.display = "block"
+      mensagem_erro.innerHTML = "(Mensagem de erro para todos os campos em branco)";
+
+      finalizarAguardar();
+      return false;
+  }
+  else {
+      setInterval(sumirMensagem, 5000)
+  }
+
+  // Enviando o valor da nova input
+  fetch("/usuarios/cadastrar", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          // crie um atributo que recebe o valor recuperado aqui
+          // Agora vá para o arquivo routes/usuario.js
+          nomeServer: nomeVar,
+          cnpjServer: CNPJVar,
+          telefone1Server: telefone1Var,
+          telefone2Server: telefone2Var,
+          emailServer: emailVar,
+          senhaServer: senhaVar,
+      })
+  }).then(function (resposta) {
+
+      console.log("resposta: ", resposta);
+
+      if (resposta.ok) {
+          alerta_erro.style.display = "flex";
+          document.body.style.overflow = 'hidden'
+          mensagem_erro.style.display = 'block';
+
+          setTimeout(() => {
+              window.location = "login.html";
+          }, "2000")
+          
+      } else {
+          throw ("Houve um erro ao tentar realizar o cadastro!");
+      }
+  }).catch(function (resposta) {
+      console.log(`#ERRO: ${resposta}`);
+  });
+
+  return false;
+}
+
+function sumirMensagem() {
+  cardErro.style.display = "flex"
+}
---
+++ Site/src/database/script-tabelas.sql
@@ -0,0 +1,133 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql - banco local - ambiente de desenvolvimento
+*/
+
+-- Criação da banco de dados do projeto AlertCenter.
+
+CREATE DATABASE AlertCenter;
+USE AlertCenter;
+
+-- Criação das tabelas conforme o DER.
+
+-- Entidade Forte 
+CREATE TABLE Empresa (
+idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+CNPJ CHAR(14),
+telefone1 CHAR(11),
+telefone2 CHAR(11),
+email VARCHAR(45), 
+-- Verificação básica do email e seus parâmetros
+CONSTRAINT chkEmail CHECK (email LIKE '%@%.%' AND email NOT LIKE '@%' and email NOT LIKE '%.'), 
+senha VARCHAR(45),
+);
+
+-- Atributo composto com mais de 2 itens - Endereço
+-- Entidade Fraca (depende da Empresa)
+CREATE TABLE Endereco (
+idEndereco INT,
+-- Relação de 1 para 1. Uma FK para uma PK (UNIQUE a FK)
+fkEmpresa INT UNIQUE,
+FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
+PRIMARY KEY (idEndereco,fkEmpresa),
+rua VARCHAR(45),
+bairro VARCHAR(45),
+numero CHAR(5),
+cep CHAR(8),
+complemento VARCHAR(45)
+);
+
+-- Entidade Forte
+CREATE TABLE DataCenter(
+idDataCenter INT PRIMARY KEY AUTO_INCREMENT,
+descricao VARCHAR(45),
+andar CHAR(2),
+fkEmpresa INT,
+FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+);
+
+-- Entidade Forte
+CREATE TABLE Rack (
+idRack INT PRIMARY KEY AUTO_INCREMENT,
+corredor INT,
+identificacao CHAR(3),
+categoria VARCHAR(45),
+fkDataCenter INT,
+FOREIGN KEY (fkDataCenter) REFERENCES DataCenter(idDataCenter)
+);
+
+-- Entidade Forte
+CREATE TABLE Sensor (
+idSensor INT PRIMARY KEY AUTO_INCREMENT,
+numeroSerie VARCHAR(10),
+descricao VARCHAR(45),
+fkRack INT,
+FOREIGN KEY (fkRack) REFERENCES Rack(idRack)
+);
+
+-- Entidade Fraca (depende do Sensor)
+CREATE TABLE Metrica (
+idMetrica INT,
+fkSensor INT,
+FOREIGN KEY (fkSensor) REFERENCES Sensor(idSensor),
+PRIMARY KEY (idMetrica,fkSensor),
+temperatura DOUBLE,
+umidade DOUBLE,
+dtMetrica DATETIME
+);
+
+/*
+comando para sql server - banco remoto - ambiente de produção
+*/
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY IDENTITY(1,1),
+	descricao VARCHAR(300)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+CREATE TABLE medida (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
+);
+
+/*
+comandos para criar usuário em banco de dados azure, sqlserver,
+com permissão de insert + update + delete + select
+*/
+
+CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
+WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
+DEFAULT_SCHEMA = dbo;
+
+EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+
+EXEC sys.sp_addrolemember @rolename = N'db_datareader',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
