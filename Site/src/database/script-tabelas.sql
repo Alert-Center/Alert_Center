12bac60d17309c4bea49292e161a544807011b5a
luizfiller <luiz.fsilva@sptech.school>
2022-11-30 21:20:31 -0300

    ADD - Comandos de criação de tabela SQL Server e criação de usuário

--- Site/src/database/script-tabelas.sql
+++ Site/src/database/script-tabelas.sql
@@ -1,10 +1,4 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
+/* Comandos para mysql - banco local - ambiente de desenvolvimento */
 
 -- Criação da banco de dados do projeto AlertCenter.
 
@@ -21,7 +15,6 @@ CNPJ CHAR(14),
 telefone1 CHAR(11),
 telefone2 CHAR(11),
 email VARCHAR(45), 
--- Verificação básica do email e seus parâmetros
 CONSTRAINT chkEmail CHECK (email LIKE '%@%.%' AND email NOT LIKE '@%' and email NOT LIKE '%.'), 
 senha VARCHAR(45)
 );
@@ -83,46 +76,98 @@ dtMetrica DATETIME
 
 -- --------------------------------------------------------------
 -- Nossa empresa irá até o DataCenter para cadastrar os dispositivos no sistema:
-insert into datacenter values (1,'South-Murilo','1',1);
+INSERT INTO datacenter VALUES 
+(1,'South-Murilo','1',1);
 
-insert into rack values 
+INSERT INTO rack VALUES 
 (1,1,'A','aberto',1);
 
-insert into sensor values 
+INSERT INTO sensor VALUES 
 (1,'0001MD','DHT-11',1);
 -- --------------------------------------------------------------
 
 -- Metricas inicias para o LIMIT 6
-insert into metrica values
-(null,1,25.0,50.0,now()),
-(null,1,25.0,50.0,now()),
-(null,1,25.0,50.0,now()),
-(null,1,25.0,50.0,now()),
-(null,1,25.0,50.0,now()),
-(null,1,25.0,50.0,now());
+INSERT INTO metrica VALUES
+(NULL,1,25.0,50.0,NOW()),
+(NULL,1,25.0,50.0,NOW()),
+(NULL,1,25.0,50.0,NOW()),
+(NULL,1,25.0,50.0,NOW()),
+(NULL,1,25.0,50.0,NOW()),
+(NULL,1,25.0,50.0,NOW());
 
 -- Agora, só ir atualizando as métricas conforme o Arduino ;), faça o teste! 
 -- OBS: mude APENAS a temperatura e umidade! na lógica o mesmo sensor está plotando em dois gráficos para fins didáticos.
-insert into Metrica values
-(null,1,35.0,20.0,now());
+INSERT INTO Metrica VALUES
+(NULL,1,35.0,20.0,NOW());
 
 
-/*
-comando para sql server - banco remoto - ambiente de produção
-*/
--- A FAZER
+/* Comandos para sql server - banco remoto - ambiente de produção */
 
-/*
-comandos para criar usuário em banco de dados azure, sqlserver,
-com permissão de insert + update + delete + select
-*/
+-- Criação de tabelas
+CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
+WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
+DEFAULT_SCHEMA = dbo;
 
--- CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
--- WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
--- DEFAULT_SCHEMA = dbo;
+EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
 
--- EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
--- @membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+EXEC sys.sp_addrolemember @rolename = N'db_datareader',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
 
--- EXEC sys.sp_addrolemember @rolename = N'db_datareader',
--- @membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+CREATE TABLE empresa (
+idEmpresa INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(45),
+CNPJ CHAR(14),
+telefone1 CHAR(11),
+telefone2 CHAR(11),
+email VARCHAR(45), 
+CONSTRAINT chkEmail CHECK (email LIKE '%@%.%' AND email NOT LIKE '@%' and email NOT LIKE '%.'), 
+senha VARCHAR(45)
+);
+
+CREATE TABLE endereco (
+idEndereco INT,
+fkEmpresa INT UNIQUE,
+FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
+PRIMARY KEY (idEndereco,fkEmpresa),
+rua VARCHAR(45),
+bairro VARCHAR(45),
+numero CHAR(5),
+cep CHAR(8),
+complemento VARCHAR(45)
+);
+
+CREATE TABLE datacenter(
+idDataCenter INT PRIMARY KEY IDENTITY(1,1),
+descricao VARCHAR(45),
+andar CHAR(2),
+fkEmpresa INT,
+FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE rack (
+idRack INT PRIMARY KEY IDENTITY(1,1),
+corredor INT,
+identificacao CHAR(3),
+categoria VARCHAR(45),
+fkDataCenter INT,
+FOREIGN KEY (fkDataCenter) REFERENCES datacenter(idDataCenter)
+);
+
+CREATE TABLE sensor (
+idSensor INT PRIMARY KEY IDENTITY(1,1),
+numeroSerie VARCHAR(10),
+descricao VARCHAR(45),
+fkRack INT,
+FOREIGN KEY (fkRack) REFERENCES rack(idRack)
+);
+
+CREATE TABLE metrica (
+idMetrica INT IDENTITY(1,1),
+fkSensor INT,
+FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor),
+PRIMARY KEY (idMetrica,fkSensor),
+temperatura DECIMAL(10,2),
+umidade DECIMAL(10,2),
+dtMetrica DATETIME
+);
