ca44410cc889df5f85fab59ee6d2da3bbb61e227
leodanave <leonardo.nave@sptech.school>
2022-12-03 14:40:48 -0300

    ADD - Chat Box da ferramenta de Help Desk

--- Site/public/js/dashboard-geral.js
+++ Site/public/js/dashboard-geral.js
@@ -292,3 +292,18 @@ const myChart_geral_umid = new Chart(
   document.getElementById('chart-geral-umid'),
   config_geral_umid
 );
+
+// Integração da ferramenta de Help Desk com o Site (Chat Box)
+//<![CDATA[
+  var ttChatLoaderS = document.createElement('script');
+  document.tomticketChatLoaderScriptVersion = 2;
+  ttChatLoaderS.src = 'https://ondata.tomticket.com/scripts-chat/chat.min.js'
+  + '?id=EP59760'
+  + '&account=3824640P16112022101310'
+  + '&autoOpen=0'
+  + '&hideWhenOffline=0'
+  + '&d=ondata'
+  + '&ts=' + new Date().getTime()
+  + '&ref=' + encodeURIComponent(document.URL);
+  document.body.appendChild(ttChatLoaderS);
+  //]]>
--- Site/src/database/config.js
+++ Site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "AlertCenter",
     user: "root",
-    password: "230103leo",
+    password: "sptech",
 };
 
 function executar(instrucao) {
