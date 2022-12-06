d22a22e06b07e9bae10efc598d878fcca4cb7d31
luizfiller <luiz.fsilva@sptech.school>
2022-12-06 10:52:39 -0300

    Delete - Diretório de site para bug fix de deploy no GitHub

--- Site/src/routes/avisos.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-var avisoController = require("../controllers/avisoController");
-
-router.post("/listarAvisos", function(req,res){
-    avisoController.listarAvisoController(req,res)
-})
-module.exports = router;
